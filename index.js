2cb9576689c011feb890f1f2126495137677422b
mail@christophermaier.com.au <mail@christophermaier.com.au>
2017-12-13 21:56:52 +1000

    User model working and saving to mLab online db.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');  // Protected API keys and settings
+require('./models/User'); // [Order of require statements] Must be loaded BEFORE ./services/passport
 require('./services/passport'); // Helper modules and business logic
-require('./models/User');
+
 
 mongoose.connect(keys.mongoURI);
 
